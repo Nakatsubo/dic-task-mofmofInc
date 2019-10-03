17c99eba27a02dbcdd9bc73e42661e89892099d1
NakatsuboYusuke <>
2019-10-03 17:27:51 +0900

    最寄り駅(access)テーブルを作成しました

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191003081219) do
+ActiveRecord::Schema.define(version: 20191003082344) do
+
+  create_table "accesses", force: :cascade do |t|
+    t.string   "route"
+    t.string   "station"
+    t.integer  "distance"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "properties", force: :cascade do |t|
     t.string   "name"
