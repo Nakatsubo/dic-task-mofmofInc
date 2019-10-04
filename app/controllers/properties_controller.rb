9b401a4d727f01b4d1b2474bfbdd6b73b83b0aee
NakatsuboYusuke <>
2019-10-04 13:52:15 +0900

    物件のMVCファイルを修正しました

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,74 +1,52 @@
 class PropertiesController < ApplicationController
-  before_action :set_property, only: [:show, :edit, :update, :destroy]
 
-  # GET /properties
-  # GET /properties.json
+  before_action :set_property, only: [:show, :edit, :update, :destroy]
+  before_action :property_params, only: [:create, :update]
+
   def index
     @properties = Property.all
   end
 
-  # GET /properties/1
-  # GET /properties/1.json
   def show
   end
 
-  # GET /properties/new
   def new
     @property = Property.new
   end
 
-  # GET /properties/1/edit
   def edit
   end
 
-  # POST /properties
-  # POST /properties.json
   def create
     @property = Property.new(property_params)
-
-    respond_to do |format|
-      if @property.save
-        format.html { redirect_to @property, notice: 'Property was successfully created.' }
-        format.json { render :show, status: :created, location: @property }
-      else
-        format.html { render :new }
-        format.json { render json: @property.errors, status: :unprocessable_entity }
-      end
+    if @property.save
+      redirect_to property_path(@property.id), notice: '物件を作成しました'
+    else
+      render :new
     end
   end
 
-  # PATCH/PUT /properties/1
-  # PATCH/PUT /properties/1.json
   def update
-    respond_to do |format|
-      if @property.update(property_params)
-        format.html { redirect_to @property, notice: 'Property was successfully updated.' }
-        format.json { render :show, status: :ok, location: @property }
-      else
-        format.html { render :edit }
-        format.json { render json: @property.errors, status: :unprocessable_entity }
-      end
+    if @property.update(property_params)
+      redirect_to property_path(@property.id), notice: '物件を更新しました'
+    else
+      render :edit
     end
   end
 
-  # DELETE /properties/1
-  # DELETE /properties/1.json
   def destroy
     @property.destroy
-    respond_to do |format|
-      format.html { redirect_to properties_url, notice: 'Property was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to properties_path, notice: '物件を削除しました'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_property
-      @property = Property.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def property_params
-      params.require(:property).permit(:name, :price, :address, :year, :note)
-    end
+  def set_property
+    @property = Property.find(params[:id])
+  end
+
+  def property_params
+    params.require(:property).permit(:name, :price, :address, :year, :note)
+  end
+
 end
--- app/models/property.rb
+++ app/models/property.rb
@@ -1,11 +1,11 @@
 class Property < ApplicationRecord
 
   #validation
-  #validates :name, presence: true
-  #validates :price, presence: true
-  #validates :address, presence: true
-  #validates :year, presence: true
-  #validates :note, presence: true
+  validates :name, presence: true
+  validates :price, presence: true
+  validates :address, presence: true
+  validates :year, presence: true
+  validates :note, presence: true
 
   # association => appoint table name
   has_many :accesses, dependent: :destroy
